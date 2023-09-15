ea9c77af39d45af550ea323612bbcb60d8a434f2
KimSungHun <kimtiger1201@naver.com>
2023-09-15 22:44:12 +0900

    .

--- Source/Shooter/ShooterGameModeBase.cpp
+++ Source/Shooter/ShooterGameModeBase.cpp
@@ -2,4 +2,21 @@
 
 
 #include "ShooterGameModeBase.h"
+//#include "Misc/DateTime.h"
 
+
+void AShooterGameModeBase::StartGame()
+{
+    if(GameState==StateOfGame::Ready){
+        SetGameState(StateOfGame::Play);
+        SetStartTime(FDateTime::Now());
+    }
+}
+
+void AShooterGameModeBase::EndGame()
+{
+    if(GameState==StateOfGame::Play){
+        SetGameState(StateOfGame::End);
+        SetStartTime(FDateTime::Now());
+    }
+}
